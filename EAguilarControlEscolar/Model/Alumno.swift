a14e4c26ef678cdc69b715f25296ae3450556fb0
EAguilar99 <e.agu.hndz@gmail.com>
2023-06-08 13:44:33 -0600

    segunda prueba

---
+++ EAguilarControlEscolar/Model/Alumno.swift
@@ -0,0 +1,29 @@
+//
+//  Alumno.swift
+//  EAguilarControlEscolar
+//
+//  Created by MacBookMBA17 on 06/06/23.
+//
+
+import Foundation
+
+struct Alumno : Codable
+{
+    var IdAlumno : Int
+    var Nombre : String
+    var ApellidoPaterno : String
+    var ApellidoMaterno : String
+    var FechaNacimiento : String?
+    var Genero : String
+    var Telefono : String
+    
+    init() {
+        self.IdAlumno = 0
+        self.Nombre = ""
+        self.ApellidoPaterno = ""
+        self.ApellidoMaterno = ""
+        self.FechaNacimiento = ""
+        self.Genero = ""
+        self.Telefono = ""
+    }
+}
